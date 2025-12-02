c471948a2c76407309f2469ea35ed08bdb9bbb66
Singh_Karminder <102573252+SinghKarminder@users.noreply.github.com>
2025-12-02 10:44:12 +0530

    Update cam.py

--- On_Edge_TFLite/Extracting_images_from_video/cam.py
+++ On_Edge_TFLite/Extracting_images_from_video/cam.py
@@ -1,20 +1,26 @@
 import cv2
 import os
 import time
+
 st = time.time()
+
 video_path = './vid1.avi'
 frames_path = './frames'
+
 video = cv2.VideoCapture(video_path)
-while True:
-image_exists, image = video.read()
 cnt = 0
-while image_exists is True:
-if not os.path.isdir(frames_path):
-os.makedirs(frames_path)
-cv2.imwrite(frames_path+'/input%03d.png'%cnt, image)
-image_exists, image = video.read()
-cnt += 1
-else:
-break
-end=time.time()
-print(end-st)
+
+while True:
+    image_exists, image = video.read()
+    if image_exists:
+        if not os.path.isdir(frames_path):
+            os.makedirs(frames_path)
+        cv2.imwrite(os.path.join(frames_path, 'input%03d.png' % cnt), image)
+        cnt += 1
+    else:
+        break
+
+video.release()
+end = time.time()
+print("Time taken:", end - st)
+
